ec0d4befcd9452721816ec40c2c81ff051bbd463
Nils <nils.dvl+git@gmail.com>
2024-11-01 11:21:50 +0100

    update github workflow to *-artifact@v4

--- Assets/Scripts/Runtime/PlayerApplication.cs
+++ Assets/Scripts/Runtime/PlayerApplication.cs
@@ -1,3 +1,15 @@
+/*
+ * Copyright (c) 2024 MotionSpell
+ * Licensed under the License terms and conditions for use, reproduction,
+ * and distribution of 5GMAG software (the “License”).
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at https://www.5g-mag.com/license .
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an “AS IS” BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
